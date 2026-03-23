fa23d840acf01fb2f449a294d36c93318e445afd
huyhandsome6996 <iamdoinb6996@gmail.com>
2026-03-23 23:40:01 +0700

    Fix settings path for linter and Render

--- core/core/wsgi.py
+++ core/core/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Sửa từ:
+
+# Thành:
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.core.settings')
 
 application = get_wsgi_application()
